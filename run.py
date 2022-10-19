b362297f4f4f7bae999f103e39e2143949d1445d
Yari-Carelli <yaricarelli@hotmail.it>
2022-10-19 09:48:29 +0000

    Create 'print_hangman(values)' function

--- run.py
+++ run.py
@@ -5,3 +5,19 @@ import os
 # Funtion to clear te terminal
 def clear():
     os.system("clear")
+
+
+# Functuion to print the hangman
+def print_hangman(values):
+    print()
+    print("\t +--------+")
+    print("\t |       | |")
+    print("\t {}       | |".format(values[0]))
+    print("\t{}{}{}      | |".format(values[1], values[2], values[3]))
+    print("\t {}       | |".format(values[4]))
+    print("\t{} {}      | |".format(values[5],values[6]))
+    print("\t         | |")
+    print("  _______________|_|___")
+    print("  `````````````````````")
+    print()
+ 
